9855d9fd5741d2b036b2872a80f81a76ff003674
Sako Hartounian <sakohartounian@yahoo.com>
2018-05-29 15:26:46 -0700

    proper tests written

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const request = require('request')
 const authHeader = {
   'auth': {
-    'bearer': 'BQBV9RR0Gh23iBtx5AUc2VdWeHIl5VTHudSjhWhDEd4MfU3yhFjmJGj60vL2XrJpP_3eWegUIHeclhD9IaNoqfC9dUaFYwA_Qc4SmoG3jKbCeCcx00ejKWSl8pbCKk7EpQKIJLIqEz5JBpnLB3na9b6EOANZXvmc5vVBU7u5pfduS5kaq_CVdCMser8gYgIij6aJzU251Dy4E5QpM-11MgjBu36UXGYbFDIAfUrcSVxgpxWGnfZ9U7hszJLgV9048A'
+    'bearer': 'BQD1n3QLCSFV4pECIk9BTz7sh46uzhSJP0eJNuz3OA76uUd9HEbOzSK1naT0WerHHyZw3_b_Hf1oGthLe6w3l5wiVdesX6mqHtDIPN2Apum8FtttvObe8Yf7aRxxBOoHjvYrOelfNGBtDmVo6DdsQDCDYAntKmqbOzjWJgPrkKc1dJ-6v-hgmto_HYd5dxLSfhRP7NXEdXMfMCe4eW3pLpKBvqIQjzPx3IQ_0AaddyxYLV5eOvJKvWSBRB7hJ4g7nw'
   }
 }
 
@@ -14,9 +14,9 @@ app.get('/', function (req, res) {
 app.get('/api/v1/search-artists/:name', (req, res) => {
   request(`https://api.spotify.com/v1/search?q=${req.params.name}&type=artist`, authHeader, (error, response, body) => {
     if (error || response.statusCode !== 200) {
-      return console.log('Error occurred: ' + response.body.message)
+      return res.json(JSON.parse(response.body))
     }
-  
+
     res.json(JSON.parse(body))
   })
 })
@@ -24,9 +24,9 @@ app.get('/api/v1/search-artists/:name', (req, res) => {
 app.get('/api/v1/artists/:id', (req, res) => {
   request(`https://api.spotify.com/v1/artists/${req.params.id}`, authHeader, (error, response, body) => {
     if (error || response.statusCode !== 200) {
-      return console.log('Error occurred: ' + response.body.message)
+      return res.json(JSON.parse(response.body))
     }
-  
+
     res.json(JSON.parse(body))
   })
 })
@@ -34,9 +34,9 @@ app.get('/api/v1/artists/:id', (req, res) => {
 app.get('/api/v1/artists/:id/related-artists', (req, res) => {
   request(`https://api.spotify.com/v1/artists/${req.params.id}/related-artists`, authHeader, (error, response, body) => {
     if (error || response.statusCode !== 200) {
-      return console.log('Error occurred: ' + response.body.message)
+      return res.json(JSON.parse(response.body))
     }
-  
+
     res.json(JSON.parse(body))
   })
 })
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,20 +1,41 @@
-var request = require('supertest');
+const request = require('supertest')
+
 describe('loading express', function () {
-  var server;
+  let server
   beforeEach(function () {
-    server = require('..');
-  });
+    server = require('..')
+  })
   afterEach(function () {
-    server.close();
-  });
+    server.close()
+  })
   it('responds to /', function testSlash(done) {
-  request(server)
-    .get('/')
-    .expect(200, done);
-  });
+    request(server)
+      .get('/')
+      .expect(200, done)
+  })
+
+  it('responds to /api/v1/search-artists/Kamelot', function testSlash(done) {
+    request(server)
+      .get('/api/v1/search-artists/Kamelot')
+      .expect(200, done)
+  })
+
+  it('responds to /api/v1/artists/7gTbq5nTZGQIUgjEGXQpOS', function testSlash(done) {
+    request(server)
+      .get('/api/v1/artists/7gTbq5nTZGQIUgjEGXQpOS')
+      .expect(200, done)
+  })
+
+  it('responds to /api/v1/artists/7gTbq5nTZGQIUgjEGXQpOS/related-artists', function testSlash(done) {
+    request(server)
+      .get('/api/v1/artists/7gTbq5nTZGQIUgjEGXQpOS/related-artists')
+      .expect(200, done)
+  })
+
+
   it('404 everything else', function testPath(done) {
     request(server)
       .get('/foo/bar')
-      .expect(404, done);
-  });
-});
+      .expect(404, done)
+  })
+})
