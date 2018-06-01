45a8c4967e677cc8b94b0c40a959eb1762817e0e
Sako Hartounian <sakohartounian@yahoo.com>
2018-06-01 09:34:57 -0700

    bearer token updated.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const axios = require('axios')
-const authBearer = 'BQD3Ia4ZG-YX2if3LHn0X1IOpvqRLKbXUeIUznsmhl7rEijLXFRCGnipq34pghx4WuSmeEE3joemJJSrI76_zaUoC5sjDskC-frLTAa4vNluJkXnGJuqz9hat-oPt7U6ZfV8jp9sLcsRvAqN_AY5nuSCEK7gVDqU5w2kBk_dMgY5PSvoK0ZAku5HQNU9zShEJjUHd6iRKz8lZhZHt3J3xPQjkFXw9Qrw2R33JnOSqIwqSaY3SlwmYj7tqIP7UI1niA'
+const authBearer = 'BQD3KfJY8P-6ob9C7UA9k75HgLsZ5LNOJDcazIN1DwKysnxxPGs2kudurFlLawfXGgxiqkKXur6v8e8AmijV6mjfQeF_hC2aSlJIH70gDDwMWl5ONVfnaa5fQbeVrC7TBMaVqQ7DuabHi9Eurt4dOcFPZfVV7JTvBJmUKFFEsZmuijkIkceHKX-hDDJ18Dd-adeB1IpkLeZW5p4pIsN4nhy4WgauDvtKi-7h7YHElPkuegeDYcTOI2KzbVsjEFk8HQ'
 const axiosInstance = axios.create({
   baseURL: 'https://api.spotify.com/v1/',
   headers: {
